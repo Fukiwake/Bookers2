2b6bfd9c6c45a7153ed3db6eb9dade3e5050526a
Fukiwake <tak0203753@gmail.com>
2021-01-08 12:47:41 +0000

    [Finish] ユーザーページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '7fc9e0cf6c59b5d7143fe9f56eaaff0f784797683bfd0ae54cf2a6bef001a95d065cebdbc3a754cf00638cccdc9d0be29db575d04aff99ad653cb7f95038ef09'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/index'
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
 end
