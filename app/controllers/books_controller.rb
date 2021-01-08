fa93cd486a08333059bffec21f9bb154778a3388
Fukiwake <tak0203753@gmail.com>
2021-01-08 06:46:23 +0000

    [Finish] 投稿機能

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,24 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+  
+  def index
+    @book = Book.new
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "homes/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books, only: [:create, :index, :show, :destroy]
 end
